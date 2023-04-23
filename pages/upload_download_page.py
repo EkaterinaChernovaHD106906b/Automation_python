0b35c6ea511a0ce3184914d6c75765ef4e4169d4
Екатерина Чернова <hd106906bqwerty@gmail.com>
2023-04-23 10:40:08 +0300

    tests were added

--- pages/upload_download_page.py
+++ pages/upload_download_page.py
@@ -1,4 +1,6 @@
+import base64
 import os
+import random
 import time
 
 from selenium.webdriver.common.by import By
@@ -11,6 +13,7 @@ class UploadDownloadPage(BasePage):
     DOWNLOAD = (By.CSS_SELECTOR, 'a[id="downloadButton"]')
     UPLOAD_PATH = (By.CSS_SELECTOR, 'p[id="uploadedFilePath"]')
     UPLOAD_BUTTON = (By.CSS_SELECTOR, 'input[id="uploadFile"]')
+    HREF_DOWNLOAD = (By.CSS_SELECTOR, 'a[id="downloadButton"]')
 
     def upload_file(self):
         file_name, path = generated_file()
@@ -18,3 +21,16 @@ class UploadDownloadPage(BasePage):
         os.remove(path)
         uploaded_file = self.element_is_present(self.UPLOAD_PATH).text
         return file_name.split('\\')[-1], uploaded_file.split('\\')[-1]
+
+    def download_file(self):
+        link = self.element_is_present(self.HREF_DOWNLOAD).get_attribute('href')
+        link_b = base64.b64decode(link)
+        path_name_file = rf'C:\Users\user\PycharmProjects\Automation_python\filetest{random.randint(1, 100)}.jpeg'
+        with open(path_name_file, 'wb+') as f:
+            offset = link_b.find(b'\xff\xd8')
+            f.write(link_b[offset:])
+            check_file = os.path.exists(path_name_file)
+            f.close()
+        os.remove(path_name_file)
+        return check_file
+
